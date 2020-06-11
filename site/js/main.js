6bbc1a54b8707742ee1b0b182b747b216d40c141
thalles <thallesgalv@gmail.com>
2020-06-11 19:45:24 -0300

    efeitos

--- site/js/main.js
+++ site/js/main.js
@@ -9,3 +9,26 @@ $(function () {
         }
     });
 })
+
+
+$(document).ready(function() {
+    $('#particles').particleground({
+      minSpeedX: 0.1,
+      maxSpeedX: 0.7,
+      minSpeedY: 0.1,
+      maxSpeedY: 0.7,
+      directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
+      directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
+      density: 10000, // How many particles will be generated: one particle every n pixels
+      dotColor: '#fff',
+      lineColor: '#fff',
+      particleRadius: 7, // Dot size
+      lineWidth: 1,
+      curvedLines: false,
+      proximity: 60, // How close two dots need to be before they join
+      parallax: false,
+      parallaxMultiplier: 5, // The lower the number, the more extreme the parallax effect
+      onInit: function() {},
+      onDestroy: function() {}
+    });
+    });
